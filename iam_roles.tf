47a8fe2ec94909ed3d0f8d36e629b3b5213eb3b7
Manthan1410 <133253672+Manthan1410@users.noreply.github.com>
2025-09-25 03:01:56 +0530

    Update iam_roles.tf

--- iam_roles.tf
+++ iam_roles.tf
@@ -4,7 +4,7 @@ resource "aws_iam_role" "s3_readonly_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action    = "sts:AssumeRole"
+      Action    = "sts:AssumeRole" 
       Effect    = "Allow"
       Principal = { Service = "ec2.amazonaws.com" }
     }]
